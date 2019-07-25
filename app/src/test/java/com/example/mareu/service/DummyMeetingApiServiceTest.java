6386cc51f1bc9d42a582344f592f67d6a2feb4d1
absyntek <absyntek.ep@gmail.com>
2019-07-25 14:40:01 +0200

    unit test

---
+++ app/src/test/java/com/example/mareu/service/DummyMeetingApiServiceTest.java
@@ -0,0 +1,67 @@
+package com.example.mareu.service;
+
+import com.example.mareu.di.DI;
+import com.example.mareu.model.Meeting;
+
+import org.hamcrest.collection.IsIterableContainingInAnyOrder;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+@RunWith(JUnit4.class)
+public class DummyMeetingApiServiceTest {
+
+    private MeetingApiService mApiService;
+    private Meeting meeting;
+    int SIZE = 3;
+
+    @Before
+    public void setup(){
+        mApiService = DI.getServiceMeet();
+    }
+
+    @Test
+    public void getMeetings() { //TODO Commencer par cette ligne SVP
+        List<Meeting> meetings = mApiService.getMeetings();
+        List<Meeting> expectedMeetings = DummyMeetingGenerator.generateMeetings();
+        assertEquals(meetings.size(),expectedMeetings.size());
+        assertThat(meetings, IsIterableContainingInAnyOrder.containsInAnyOrder(expectedMeetings.toArray()));
+    }
+
+    @Test
+    public void addMeeting() {
+        meeting = new Meeting(null,"dsfs",
+                "dfdsdfs",null,1234);
+        mApiService.addMeeting(meeting);
+        assertTrue(mApiService.getMeetings().contains(meeting));
+    }
+
+    @Test
+    public void deletMeeting() {
+        meeting = mApiService.getMeetings().get(3);
+        mApiService.deletMeeting(meeting);
+        assertFalse(mApiService.getMeetings().contains(meeting) );
+    }
+
+    @Test
+    public void getMeetingID() {
+        meeting = mApiService.getMeetings().get(3);
+        assertEquals(3,mApiService.getMeetingID(meeting));
+    }
+
+    @Test
+    public void getMeetingPoints() {
+        List<String> toTest = Arrays.asList(mApiService.getMeetingPoints());
+        assertEquals(10,toTest.size());
+    }
+}
